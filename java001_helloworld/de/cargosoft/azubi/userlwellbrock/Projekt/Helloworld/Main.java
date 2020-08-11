329ead8f0e18eecf7fd2dbef155b2b7c6270812f
lwellbrock <lwellbrock@cargosoft.de>
2020-08-11 12:05:43 +0000

    Java001 und Java003 Verzeichnisse hinzugefügt

---
+++ java001_helloworld/de/cargosoft/azubi/userlwellbrock/Projekt/Helloworld/Main.java
@@ -0,0 +1,12 @@
+package de.cargosoft.azubi.userlwellbrock.Projekt.Helloworld;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+    System.out.println("Hello");   
+
+    System.out.println("World");                                                                                                                                                                                                              
+		       }
+ 
+}
---
+++ java003_kommentieren/de/cargosoft/azubi/userlwellbrock/Projekt/Helloworld/Main.java
@@ -0,0 +1,17 @@
+package de.cargosoft.azubi.userlwellbrock.Projekt.Helloworld;
+
+// Das ist ein Kommentar
+
+/**
+ * Das ist ein anderer Kommentar
+ */
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+    System.out.println("Hello");   
+
+    System.out.println("World");                                                                                                                                                                                                              
+		       }
+ 
+}
